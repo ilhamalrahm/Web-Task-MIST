f672caa7b7fdc2f0e1fa7979aa7bfb6a591e9df3
Ilham-Rahm <ilhamalrahm@gmail.com>
2021-08-22 14:19:16 +0530

    added alert for button

--- practice.js
+++ practice.js
@@ -2,3 +2,13 @@ function clickMe()
 {
     window.scrollTo(0,0);
 }
+
+function alertCall()
+{
+    document.getElementById("spanalert").style.visibility="visible";
+}
+
+function alertClose()
+{
+    document.getElementById("spanalert").style.visibility="hidden";
+}
