a9bb3d8c6840a49e2410a6053716d16e8e0d8e5d
Ilham-Rahm <ilhamalrahm@gmail.com>
2021-08-25 17:22:13 +0530

    added some javascript on account button

--- practice.js
+++ practice.js
@@ -12,3 +12,14 @@ function alertClose()
 {
     document.getElementById("spanalert").style.visibility="hidden";
 }
+function accountAlert()
+    {
+       x=document.getElementById("account");
+       x.addEvenListener("click", alertFunction());
+    }
+
+
+    function alertFunction()
+        {
+            alert("Sorry, Account doesn't exist");
+        }
